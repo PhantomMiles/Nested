b8e20b89f160fa32b0ea8a69e955ee175c5ff1e9
Josephine Elonye <phantommiles@gmail.com>
2025-10-18 11:03:18 +0100

    tweaked mobile navbar

---
+++ .history/js/nav_20251017101159.js
@@ -0,0 +1,26 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() { myFunction() };
+
+// Get the header
+var header = document.getElementById("myHeader");
+var mobile_nav = document.getElementById("navbar-mobile");
+
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+        mobile_nav.classList.add("sticky");
+        header.style.backgroundColor = "#00008B";
+        // header.style.marginTop = "10px";
+        header.style.transition = "ease-in 0.2s";
+
+    } else {
+        header.classList.remove("sticky");
+        header.style.backgroundColor = "transparent";
+
+    }
+}
